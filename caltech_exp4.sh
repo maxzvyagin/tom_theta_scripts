15fc904d8576dcb8d71f477d926b0715f3f28b88
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-25 15:37:09 -0800

    quick dir update

--- caltech_exp4.sh
+++ caltech_exp4.sh
@@ -1,4 +1,4 @@
 source /home/mzvyagin/.bashrc
 lustre
-cd resilient_alexnet/
+cd resilient_alexnet/resilient_alexnet/
 python alexnet_tune.py -o results/caltech_exp4.csv -n 0 -t 8 -j standard.json -p caltech_exp4 --maximize_convergence -m caltech
