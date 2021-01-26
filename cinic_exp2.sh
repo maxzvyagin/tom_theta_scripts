d953ac08f885eace1f6709e1761f3663f3f8756d
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-25 21:36:15 -0800

    fixed sourcing

--- cinic_exp2.sh
+++ cinic_exp2.sh
@@ -1,4 +1,4 @@
-source /home/mzvyagin/.bashrc
+#source /home/mzvyagin/.bashrc
 lustre
 cd resilient_alexnet/resilient_alexnet/
 python alexnet_tune.py -o results/cinic_exp2.csv -n 0 -t 8 -j standard.json -p cinic_exp2 --maximize_difference -m cinic
--- fashion_exp4.sh
+++ fashion_exp4.sh
@@ -1,4 +1,4 @@
-source /home/mzvyagin/.bashrc
+#source /home/mzvyagin/.bashrc
 lustre
 cd resilient_alexnet/resilient_alexnet/
 python alexnet_tune.py -o results/fashion_exp4.csv -n 0 -t 8 -j standard.json -p fashion_exp4 --maximize_convergence
